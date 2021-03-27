db43ab5fd8cebdb5afee9ed465930310a7ea1982
nikoada <nikushac@gmail.com>
2021-03-27 19:20:44 +0100

    feat: add voice command to cahnge display mode

--- public/script.js
+++ public/script.js
@@ -5,26 +5,42 @@ import { itemsList } from "./itemsList.js"
 let oElements = {}
 
 itemsList.forEach(item => {
-  oElements[item] = new Item(item).createDivElement()
+  let nameCapitalized = item.charAt(0).toUpperCase() + item.slice(1)
+  oElements[item] = new Item(nameCapitalized).createDivElement()
 })
 
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 
 // setup SpeechRecognition
-const recognition = new SpeechRecognition();
-recognition.interimResults = true;
-recognition.lang = 'en-US';
+const recognition = new SpeechRecognition()
+recognition.interimResults = true
+recognition.lang = 'en-US'
 
 // waiting for speech results
 recognition.addEventListener('result', event => {
-  const transcript = event.results[0][0].transcript;
-
+  const transcript = event.results[0][0].transcript
+  console.log(transcript)
   // check if the voice input has ended
-  if (event.results[0].isFinal) {
-    console.log(transcript);
+  if (event.results[0].isFinal && oElements[transcript]) {
     document.getElementById("mainContainer").appendChild(oElements[transcript])
   }
+
+  if (transcript === "table") {
+    addClass('table')
+  }
+
+  if (transcript === "list") {
+    addClass('list')
+  }
+
 });
 
-recognition.addEventListener('end', recognition.start);
-recognition.start();
+let addClass = className => {
+  let divs = document.getElementsByTagName('div')
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].setAttribute( 'class', className)
+    }
+}
+
+recognition.addEventListener('end', recognition.start)
+recognition.start()
