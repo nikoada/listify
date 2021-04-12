aa6dad2d4e4f90a5df25be5b61902fbe5baf5da6
nikoada <nikushac@gmail.com>
2021-04-12 17:21:36 +0200

    feat: add local storage

--- public/script.js
+++ public/script.js
@@ -12,11 +12,35 @@ const startButton = document.createElement('button')
 
 startButton.onclick = () =>  recognition.start()
 
-startButton.innerHTML = 'Listen'
+startButton.innerHTML = 'Add'
 startButton.style = 'width: 100%; height: 5rem; font-size: 2rem;'
 
 let container = document.getElementById("mainContainer")
 document.body.appendChild(startButton)
+
+let getLocalStorage = (lsKey, className, containerElement) => {
+  let savedUserList = localStorage.getItem(lsKey)
+  let arrSavedList = savedUserList.split(',')
+  arrSavedList.forEach( item => {
+    
+    if (item.split('-')[1] === 'selected') {
+      let newElement = new className(item.split('-')[0]).createDivElement()
+      newElement.style.backgroundColor = 'rgb(161, 201, 53)'
+      containerElement.appendChild(newElement)
+      return
+    }
+
+    let newElement = new className(item).createDivElement()
+      if (containerElement.hasChildNodes()) {
+        containerElement.insertBefore(newElement, container.childNodes[0])
+        return
+      }
+      containerElement.appendChild(newElement)
+  })
+}
+
+getLocalStorage('itemElements', Item, container)
+
 
 recognition.addEventListener('result', event => {
   const transcript = event.results[0][0].transcript
@@ -25,22 +49,21 @@ recognition.addEventListener('result', event => {
   // check if the voice input has ended
   if (event.results[0].isFinal && itemsList.includes(transcript)) {
     let elAlreadyExists = [... container.childNodes].filter(item => item.id === `id${transcript}`).length
-    if (elAlreadyExists) {
+    if (elAlreadyExists !== 0) {
       alert("Already in the list!")
       return
     }
 
     let newElement = new Item(transcript).createDivElement()
-    if (newElement.style.backgroundColor === 'rgb(161, 201, 53)') {
-      alert('Item is already taken!')
-      return
-    }
+    let storedList = localStorage.getItem('itemElements').split(',')
+    storedList.push(transcript)
+    localStorage.setItem('itemElements', [... storedList])
 
     if (container.hasChildNodes()) {
       container.insertBefore(newElement, container.childNodes[0])
       return
     }
-
-      container.appendChild(newElement)
+    
+    container.appendChild(newElement)
   }
 });
